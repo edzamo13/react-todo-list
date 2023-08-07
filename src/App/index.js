33ac53c05f758b97296e197136f79746cf126724
edzamo13 <edzamo13@gmail.com>
2023-08-07 17:20:37 -0500

    change the composition app Appui

--- src/App/index.js
+++ src/App/index.js
@@ -1,63 +1,13 @@
 import React from 'react';
-import {TodoProvider} from '../TodoContext'
-
-
-/**
- * 
- * A common pattern in React is for a component to return multiple elements. Fragments let you group a list of children without adding extra nodes to the DOM.
-
-render() {
-  return (
-    <React.Fragment>
-      <ChildA />
-      <ChildB />
-      <ChildC />
-    </React.Fragment>
-  );
-
- * 
- */
+import { TodoList } from '../TodoList';
+import { AppUI } from './AppUI';
+import { TodoProvider } from '../TodoContext';
 
 function App() {
-
-    const [state, setState] = React.useState('State search ')
-
     return (
-
-        <React.Fragment>
-            <TodoHeader>
-                <TodoCounter/>
-                <TodoSearch/>
-            </TodoHeader>
-            <TodoList>
-                <TodoItem state={state}/>
-            </TodoList>
-
-        </React.Fragment>
-
+      <TodoProvider>
+        <AppUI />
+      </TodoProvider>
     );
-}
-function TodoHeader({children}) {
-    return (
-        <header> {children} </header>
-    )
-}
-
-function TodoList({children}) {
-    return (
-        <section className='TodoList-container'>
-            {children} </section>
-    )
-}
-function TodoCounter() { // const [state] = React.useContext(TodoProvider)
-    return <p>TodoCounter</p>
-}
-function TodoSearch() {
-    return <p>TodoSearch</p>
-}
-function TodoItem({state}) {
-    return <p>TodoItem: {state}</p>
-}
-
-
+  }
 export default App;
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
-function TodoCounter() {
-  const {
-    completedTodos,
-    totalTodos,
-  } = React.useContext(TodoContext);
-  
-  return (
-    <h1 className="TodoCounter">
-      You have completed <span>{completedTodos}</span> of <span>{totalTodos}</span> TODO's
-    </h1>
-  );
+function TodoCounter({totalTodos, completedTodos}) {
+
+    return (
+        <h1 className="TodoCounter">
+            You have completed
+            <span>{completedTodos}</span>
+            of
+            <span>{totalTodos}</span>
+            TODO's
+        </h1>
+    );
 }
 
-export { TodoCounter };
+export {
+    TodoCounter
+};
