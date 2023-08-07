f77e01b8761084691ed851a4600e427a34d3d5e6
edzamo13 <edzamo13@gmail.com>
2023-08-07 16:02:24 -0500

    Fragment and desing new aplication

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,63 @@
 import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
+import {TodoProvider} from '../TodoContext'
+
+
+/**
+ * 
+ * A common pattern in React is for a component to return multiple elements. Fragments let you group a list of children without adding extra nodes to the DOM.
+
+render() {
+  return (
+    <React.Fragment>
+      <ChildA />
+      <ChildB />
+      <ChildC />
+    </React.Fragment>
+  );
+
+ * 
+ */
 
 function App() {
-  return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  );
+
+    const [state, setState] = React.useState('State search ')
+
+    return (
+
+        <React.Fragment>
+            <TodoHeader>
+                <TodoCounter/>
+                <TodoSearch/>
+            </TodoHeader>
+            <TodoList>
+                <TodoItem state={state}/>
+            </TodoList>
+
+        </React.Fragment>
+
+    );
 }
+function TodoHeader({children}) {
+    return (
+        <header> {children} </header>
+    )
+}
+
+function TodoList({children}) {
+    return (
+        <section className='TodoList-container'>
+            {children} </section>
+    )
+}
+function TodoCounter() { // const [state] = React.useContext(TodoProvider)
+    return <p>TodoCounter</p>
+}
+function TodoSearch() {
+    return <p>TodoSearch</p>
+}
+function TodoItem({state}) {
+    return <p>TodoItem: {state}</p>
+}
+
 
 export default App;
