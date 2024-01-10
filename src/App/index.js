49cfd499bb37165a0981f7ff5bd36a3fc2170d04
ezamoram <ezamoram@pichincha.com>
2024-01-10 08:31:42 -0500

    add project

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,93 @@
 import React from 'react';
-import { TodoList } from '../TodoList';
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContext';
+import {TodoList} from '../TodoList';
+import {TodoCounter} from '../TodoCounter';
+import {TodoSearch} from '../TodoSearch';
+import {TodoItem} from '../TodoItem';
+import {TodosLoading} from '../TodosLoading';
+import {TodosError} from '../TodosError';
+import {EmptyTodos} from '../EmptyTodos';
+import {CreateTodoButton} from '../CreateTodoButton';
+import {TodoForm} from '../TodoForm';
+import {Modal} from '../Modal';
+import TodoHeader from '../TodoHeader';
+import {useTodos} from './UseTodos';
 
 function App() {
+    const {
+      error,
+      loading,
+      searchedTodos,
+      completeTodo,
+      deleteTodo,
+      openModal,
+      setOpenModal,
+      totalTodos,
+      completedTodos,
+      searchValue,
+      setSearchValue,
+      addTodo,
+    } = useTodos();
+    
     return (
-      <TodoProvider>
-        <AppUI />
-      </TodoProvider>
-    );
-  }
+      <React.Fragment>
+        <TodoHeader>
+          <TodoCounter
+            totalTodos={totalTodos}
+            completedTodos={completedTodos}
+          />
+          <TodoSearch
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+        </TodoHeader>
+  
+        <TodoList
+        error={error}
+        loading={loading}
+        totalTodos={totalTodos}
+        searchedTodos={searchedTodos}
+        searchText={searchValue}
+        onError={() => <TodosError />}
+        onLoading={() => <TodosLoading />}
+        onEmptyTodos={() => <EmptyTodos />}
+        onEmptySearchResults={
+          (searchText) => <p>No hay resultados para {searchText}</p>
+        }
+        // render={todo => (
+        //   <TodoItem
+        //     key={todo.text}
+        //     text={todo.text}
+        //     completed={todo.completed}
+        //     onComplete={() => completeTodo(todo.text)}
+        //     onDelete={() => deleteTodo(todo.text)}
+        //   />
+        // )}
+      >
+        {todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )}
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm
+            addTodo={addTodo}
+            setOpenModal={setOpenModal}
+          />
+        </Modal>
+      )}
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
+  );
+}
+
 export default App;
--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,49 +1,40 @@
 import React from 'react';
-import { TodoContext } from '../TodoContext';
 import './TodoForm.css';
 
-function TodoForm() {
-  const {
-    addTodo,
-    setOpenModal,
-  } = React.useContext(TodoContext);
-  const [newTodoValue, setNewTodoValue] = React.useState('');
+function TodoForm({addTodo, setOpenModal}) {
+    
+    const [newTodoValue, setNewTodoValue] = React.useState('');
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    addTodo(newTodoValue);
-    setOpenModal(false);
-  };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
 
-  const onCancel = () => {
-    setOpenModal(false);
-  };
+    const onCancel = () => {
+        setOpenModal(false);
+    };
 
-  const onChange = (event) => {
-    setNewTodoValue(event.target.value);
-  };
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    };
 
-  return (
-    <form onSubmit={onSubmit}>
-      <label>Write you new TODO </label>
-      <textarea
-        placeholder=" Add yur item "
-        value={newTodoValue}
-        onChange={onChange}
-      />
-      <div className="TodoForm-buttonContainer">
-        <button
-          type="button"
-          className="TodoForm-button TodoForm-button--cancel"
-          onClick={onCancel}
-        >Cancel</button>
-        <button
-          type="submit"
-          className="TodoForm-button TodoForm-button--add"
-        >Add</button>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={onSubmit}>
+            <label>Write you new TODO
+            </label>
+            <textarea placeholder=" Add yur item "
+                value={newTodoValue}
+                onChange={onChange}/>
+            <div className="TodoForm-buttonContainer">
+                <button type="button" className="TodoForm-button TodoForm-button--cancel"
+                    onClick={onCancel}>Cancel</button>
+                <button type="submit" className="TodoForm-button TodoForm-button--add">Add</button>
+            </div>
+        </form>
+    );
 }
 
-export { TodoForm };
+export {
+    TodoForm
+};
